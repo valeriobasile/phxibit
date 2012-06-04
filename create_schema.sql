497982e03d5b7b8d999ba3c9ffd43c478f49cfe7
Valerio Basile <valeriobasile@gmail.com>
2012-06-04 19:43:23 +0200

    new DB schema: added 'page' table

--- create_schema.sql
+++ create_schema.sql
@@ -1,17 +1,42 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.10.1deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jun 04, 2012 at 07:41 PM
+-- Server version: 5.5.22
+-- PHP Version: 5.3.10-1ubuntu3.1
+
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
+--
+-- Database: `phxibit`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
 
 CREATE TABLE IF NOT EXISTS `category` (
   `id` int(11) NOT NULL,
   `name` text NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `exhibition`
+--
 
 CREATE TABLE IF NOT EXISTS `exhibition` (
   `id` int(11) NOT NULL,
@@ -20,6 +45,13 @@ CREATE TABLE IF NOT EXISTS `exhibition` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `link`
+--
+
 CREATE TABLE IF NOT EXISTS `link` (
   `id` int(11) NOT NULL,
   `url` text NOT NULL,
@@ -27,10 +59,46 @@ CREATE TABLE IF NOT EXISTS `link` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `news`
+--
+
 CREATE TABLE IF NOT EXISTS `news` (
   `text` text NOT NULL,
   `link` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `page`
+--
+
+CREATE TABLE IF NOT EXISTS `page` (
+  `id` varchar(50) NOT NULL,
+  `active` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `page`
+--
+
+INSERT INTO `page` (`id`, `active`) VALUES
+('biography', 1),
+('contacts', 0),
+('exhibitions', 1),
+('links', 0),
+('publications', 0),
+('works', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `publication`
+--
 
 CREATE TABLE IF NOT EXISTS `publication` (
   `id` int(11) NOT NULL,
@@ -42,12 +110,24 @@ CREATE TABLE IF NOT EXISTS `publication` (
   KEY `category` (`category`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `topic`
+--
+
 CREATE TABLE IF NOT EXISTS `topic` (
   `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
   `description` text NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `work`
+--
 
 CREATE TABLE IF NOT EXISTS `work` (
   `id` int(11) NOT NULL,
@@ -60,10 +140,19 @@ CREATE TABLE IF NOT EXISTS `work` (
   KEY `topic` (`topic`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Constraints for dumped tables
+--
 
+--
+-- Constraints for table `publication`
+--
 ALTER TABLE `publication`
   ADD CONSTRAINT `publication_ibfk_2` FOREIGN KEY (`category`) REFERENCES `category` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
+--
+-- Constraints for table `work`
+--
 ALTER TABLE `work`
   ADD CONSTRAINT `work_ibfk_2` FOREIGN KEY (`topic`) REFERENCES `topic` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
